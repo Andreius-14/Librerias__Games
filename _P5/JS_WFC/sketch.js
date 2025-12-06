182919df3a5b0f69da97cd09792090b7206bbc07
Andreius_14 <Andreius_14@outlook.com>
2025-12-05 20:43:03 -0500

    (add) Comentarios

--- _P5/JS_WFC/sketch.js
+++ _P5/JS_WFC/sketch.js
@@ -1,6 +1,9 @@
 /* eslint indent: "off" */
 /* eslint-disable space-before-function-paren */
 
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                        Variables                        │
+//          ╰─────────────────────────────────────────────────────────╯
 const tiles = []
 let grid = []
 const DIM = 8
@@ -12,30 +15,46 @@ const DOWN = 3
 const LEFT = 4
 
 const rules = [
+    //  ╭─────────────────╮
+    //  │      blank      │
+    //  ╰─────────────────╯
     [
         [BLANK, UP],
         [BLANK, RIGHT],
         [BLANK, DOWN],
         [BLANK, LEFT]
     ],
+    //  ╭─────────────────╮
+    //  │        UP       │
+    //  ╰─────────────────╯
     [
         [RIGHT, LEFT, DOWN],
         [LEFT, UP, DOWN],
         [BLANK, DOWN],
         [RIGHT, UP, DOWN]
     ],
+    //  ╭─────────────────╮
+    //  │      Right      │
+    //  ╰─────────────────╯
+
     [
         [RIGHT, LEFT, DOWN],
         [LEFT, UP, DOWN],
         [RIGHT, LEFT, UP],
         [BLANK, LEFT]
     ],
+    //  ╭─────────────────╮
+    //  │      Down       │
+    //  ╰─────────────────╯
     [
         [BLANK, UP],
         [LEFT, UP, DOWN],
         [RIGHT, LEFT, UP],
         [RIGHT, UP, DOWN]
     ],
+    //  ╭─────────────────╮
+    //  │       Left      │
+    //  ╰─────────────────╯
     [
         [RIGHT, LEFT, DOWN],
         [BLANK, RIGHT],
@@ -44,6 +63,9 @@ const rules = [
     ]
 ]
 
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                        Funciones                        │
+//          ╰─────────────────────────────────────────────────────────╯
 // Array de Imagen
 function preload() {
     tiles[0] = loadImage('./tiles/blank.png')
@@ -69,12 +91,15 @@ function setup() {
     //    grid[0].options = [BLANK, UP]
 }
 
+// Estás dejando en arr solo los valores que están en valid
 function checkValid(arr, valid) {
     for (let i = arr.length - 1; i >= 0; i--) {
-        // VALID: [BLANK, RIGHT]
         // ARR: [BLANK, UP, RIGHT, DOWN, LEFT]
-        // result in removing UP, DOWN, LEFT
+        // VALID: [BLANK, RIGHT]
+        // result :[BLANK, RIGHT]
         const element = arr[i]
+
+        // ARR - VALID == ARRAY MAS PEQUEÑO
         if (!valid.includes(element)) {
             arr.splice(i, 1)
         }
@@ -84,6 +109,9 @@ function mousePressed() {
     redraw()
 }
 
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                          Bucle                          │
+//          ╰─────────────────────────────────────────────────────────╯
 
 function draw() {
     background(0)
@@ -91,14 +119,16 @@ function draw() {
     const w = width / DIM
     const h = height / DIM
 
-    // Recorre --- el Grid
+    //          ╭─────────────────────────────────────────────────────────╮
+    //          │                    Draw -  Rrellena Celda               │
+    //          ╰─────────────────────────────────────────────────────────╯
     for (let j = 0; j < DIM; j++) {
         for (let i = 0; i < DIM; i++) {
             const cell = grid[i + j * DIM]
 
             if (cell.collapsed) {
                 // traduce la unica opcion que posee a Imagen
-                let index = cell.options[0]
+                const index = cell.options[0]
                 image(tiles[index], i * w, j * h, w, h)
             } else {
                 fill(0)
@@ -108,107 +138,123 @@ function draw() {
         }
     }
 
+    //          ╭─────────────────────────────────────────────────────────╮
+    //          │                  Logica -  Entropia Minima              │
+    //          ╰─────────────────────────────────────────────────────────╯
 
-    // Pick cell with least entropy
+    // Duplica
     let gridCopy = grid.slice()
-    gridCopy = gridCopy.filter((a) => !a.collapsed)
-    // console.table(grid);
-    // console.table(gridCopy) // Ordena -- Menor a Mayor -- grid[].options
 
+    //  Casillas sin Imagen
+    gridCopy = gridCopy.filter((a) => !a.collapsed)
+
+    // console.table(grid);
+    // console.table(gridCopy)
+
+    // Condificonal : Finaliza Si todo esta lleno
     if (gridCopy.length == 0) {
         return
     }
 
+    // Ordena -- Menor a Mayor
     gridCopy.sort((a, b) => {
         return a.options.length - b.options.length
     })
 
-    // Num de Obsiones (La menor posible)
+    // Selecciona > la array[0] cantidad de Opciones
     const len = gridCopy[0].options.length
     let stopIndex = 0
-    // recorre --- Busca la primera Casilla + con un numero mayor de Opciones
+
+    // Filtra el array: Mantiene solo las opciones-grid con la menor longitud (mínima entropía).
     for (let i = 1; i < gridCopy.length; i++) {
         if (gridCopy[i].options.length > len) {
             stopIndex = i
             break
         }
     }
-    // Filtra el array: Mantiene solo las opciones con la menor longitud (mínima entropía).
     if (stopIndex > 0) gridCopy.splice(stopIndex)
 
-    //---------------------------------
-    // [Entropia Minima Establecida]
-    //---------------------------------
+    //          ╭─────────────────────────────────────────────────────────╮
+    //          │                    Aplicando Entriopia                  │
+    //          ╰─────────────────────────────────────────────────────────╯
 
     // Seleccion Random - []
     const cell = random(gridCopy)
-    cell.collapsed = true
-    // Seleccion Random - pic
     const pick = random(cell.options)
+
+    cell.collapsed = true
     cell.options = [pick]
 
     // Mensaje
     console.table(grid)
     // console.log(gridCopy)
 
-
+    //          ╭─────────────────────────────────────────────────────────╮
+    //          │               Calcula el Siguiente Estado               │
+    //          ╰─────────────────────────────────────────────────────────╯
     const nextGrid = []
     for (let j = 0; j < DIM; j++) {
         for (let i = 0; i < DIM; i++) {
-            const index = i + j * DIM
+            const index = i + (j * DIM)
+
+            const celda = {
+                up: grid[i + (j - 1) * DIM],
+                right: grid[i + 1 + j * DIM],
+                down: grid[i + (j + 1) * DIM],
+                left: grid[i - 1 + j * DIM]
+            }
+
             if (grid[index].collapsed) {
+                // ────────────── Guardando Estado ──────────────
                 nextGrid[index] = grid[index]
             } else {
-                let options = [BLANK, UP, RIGHT, DOWN, LEFT]
+                const options = [BLANK, UP, RIGHT, DOWN, LEFT]
+
+                // ──────────────── recorre lados ────────────────
                 // Look up
                 if (j > 0) {
-                    let up = grid[i + (j - 1) * DIM]
+                    const up = celda.up
                     let validOptions = []
 
-                    for (let option of up.options) {
-                        let valid = rules[option][2]
+                    for (const option of up.options) {
+                        const valid = rules[option][2]
                         validOptions = validOptions.concat(valid)
                     }
                     checkValid(options, validOptions)
-
                 }
                 // Look right
                 if (i < DIM - 1) {
-                    let right = grid[i + 1 + j * DIM]
+                    const right = celda.right
                     let validOptions = []
 
-                    for (let option of right.options) {
-                        let valid = rules[option][3]
+                    for (const option of right.options) {
+                        const valid = rules[option][3]
                         validOptions = validOptions.concat(valid)
                     }
                     checkValid(options, validOptions)
-
                 }
                 // Look down
                 if (j < DIM - 1) {
-                    let down = grid[i + (j + 1) * DIM]
+                    const down = celda.down
                     let validOptions = []
 
-                    for (let option of down.options) {
-                        let valid = rules[option][0]
+                    for (const option of down.options) {
+                        const valid = rules[option][0]
                         validOptions = validOptions.concat(valid)
                     }
                     checkValid(options, validOptions)
-
-
                 }
 
                 // Look left
                 if (i > 0) {
-                    let left = grid[i - 1 + j * DIM]
+                    const left = celda.left
                     let validOptions = []
 
-                    for (let option of left.options) {
-                        let valid = rules[option][1]
+                    for (const option of left.options) {
+                        const valid = rules[option][1]
                         validOptions = validOptions.concat(valid)
                     }
                     checkValid(options, validOptions)
-
                 }
 
                 nextGrid[index] = {
